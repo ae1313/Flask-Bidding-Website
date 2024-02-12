291810feb507651f227313416a98ec3bea61ff43
Aden <adenisahabib@gmail.com>
2024-02-11 18:13:42 -0800

    User Auth, started admin system, ui a little less bad

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect, session
 import pymongo
 import json
 from flask_bcrypt import Bcrypt
@@ -10,28 +10,37 @@ client = pymongo.MongoClient(os.getenv("MONGO_URI"))
 db = client['flaskAppTest']
 
 app = Flask(__name__)
+app.secret_key = "VERY VERY SECRET DO NOT SHARE PLS :)"
 bcrypt = Bcrypt(app)
+
+def mongodo(a):
+    exec(a)
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/cart')
-def cart():
-    items = []
-    cursor = db.Groceries.find()
-    for s in cursor:
-        items.append(s)
+@app.route('/browse')
+def browse():
+    if session.get("email"):
+        items = []
+        cursor = db.Groceries.find()
+        for s in cursor:
+            items.append(s)
     
-    for i in items:
-        for j in i:
-            if j == "price":
-                i[j]=float(i[j])
-    print(items)
-    
-    return render_template("cart.html", items=items)
+        for i in items:
+            for j in i:
+                if j == "price":
+                    i[j]=float(i[j])
+        return render_template("cart.html", items=items)
+    else:
+        return redirect("/signin")
 
 @app.route('/car')
 def car():
+    if not session.get("email"):
+        return redirect("/signin")
+    
     if not request.args.get('id') == None:
         page = request.args.get('id')
         print(page)
@@ -51,24 +60,29 @@ def bidding():
         bid = int(data["data"])
         bids = s['bidding']
         min = s["price"]/2
+        username = db.users.find_one({"email": session["email"]})["username"]
         print(s)
         print(data)
         
         if bids == [] and bid > min:
             
-            db.Groceries.update_one({"pageID":id}, {"$push":{"bidding":{"name":"adam","bid":bid}}})
-            return "True"
+            db.Groceries.update_one({"pageID":id}, {"$push":{"bidding":{"name":username,"bid":bid}}})
+            db.users.update_one({"email":session["email"]},{"$push":{"bids":{"amount":bid,"id":data["id"],"car":db.Groceries.find_one({"pageID":data["id"]})["name"]}}})
+            return "Good"
         elif bids == []:
             return "False"
         elif bids[0]["bid"] < bid:
-            return "True"
+            db.Groceries.update_one({"pageID":data["id"]}, {"$push":{"bidding":{"$each":[{"name":username,"bid":bid}],"$position":0}}})
+            db.users.update_one({"email":session["email"]},{"$push":{"bids":{"amount":bid,"id":data["id"],"car":db.Groceries.find_one({"pageID":data["id"]})["name"]}}})
+
+            return "Succeed"
         
         
         # # if s["bidding"] == [] and data["data"] > s["price"]/2:
         # if bids == [] and info > min:
         #     print('hi')
         # # TODO: GITHUB
-        # db.groceried.update_one({"pageID":data["id"]}, {"$push":{"bidding":{"name":"adam","bid":data["data"]}}})
+        
         return "Big Fail"
 
 @app.route('/signup',methods=["POST","GET"])
@@ -78,6 +92,7 @@ def signup():
     if request.method == "POST":
         if not db.users.find_one({"email":request.form.get("email")}):
             user=dict(request.form)
+            user["admin"]=False
             user["password"] = bcrypt.generate_password_hash(user["password"]).decode("utf-8")
             db.users.insert_one(user)
             return redirect("\signin")
@@ -98,11 +113,15 @@ def login():
             return(redirect('/signin?e=1'))
         
         
-        if db.users.find_one({"email":request.form.get("email")}):
+        if db.users.find_one({"email":email}):
             canidate = request.form.get("password")
-            hash = db.users.find_one({"email": request.form.get("email")})["password"]
+            hash = db.users.find_one({"email": email})["password"]
             if bcrypt.check_password_hash(hash, canidate):  
-                return redirect("/cart")
+                session["email"] = email
+                print(session.get("email"))
+                return redirect("/browse")
+                
+
             else: 
                 return redirect("/signin?e=1")
         else:
@@ -110,12 +129,41 @@ def login():
             return redirect("/signin?e=1")
         return redirect("/signin?e=1")
 
+@app.route('/session_check',methods=["GET"])
+def session_check():
+    return session.get("email")
+
+@app.route('/signout',methods=["GET"])
+def sign_out():
+    session.pop('email', None)
+    print(type(session.get("email")))
+    return redirect("/signin")
+
+@app.route('/dash', methods=["GET"])
+def dash():
+    # if not session.get("email"):
+    #     return redirect("/signin")
+    user = db.users.find_one({"email": session["email"]})
+    return(render_template("dash.html",user=user))
+    print(user)
+    if False:
+    # if user["bids"]:
+        print(user)
+        # bids= user["bids"]
+        # return render_template("dash.html",bids)
+    else:
+        return redirect('/browse')
+    
+@app.route('/admin',methods=["GET"])
+def admin():
+    pass
+    return render_template("index.html")
+
 
     
 if __name__ == "__main__":
     app.debug=True
     app.run()
-    print("hi")
 
 
 client.close()
